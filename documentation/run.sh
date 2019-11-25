026d355e33fb566ece4b98c207e01911f8687456
Marco-Israel <Marco-Israel@gmx.de>
2019-11-25 15:53:56 +0100

    edit run.sh

--- documentation/run.sh
+++ documentation/run.sh
@@ -1,1 +1,17 @@
-docker run --name latex -it --rm -v $PWD:/latex/input latex
+#!/bin/bash
+
+TASK="latex"
+VERSION="latest"
+
+if ( ! systemctl -q is-active docker.servie )
+then
+    sudo systemctl start docker
+fi
+
+
+if ( $# -gt 0 )
+then
+    docker run --name $TASK -it --rm -v $PWD:/$TASK/input $TASK:$VERSION
+else
+    docker run --name $TASK -it --rm -v $PWD:/$TASK/input $TASK:$VERSION $@
+fi
