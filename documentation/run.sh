132fffd807593711ed8b454c5d694ed801dc9d6b
Marco-Israel <Marco-Israel@gmx.de>
2020-01-15 15:07:45 +0100

    add alternatives

--- documentation/run.sh
+++ documentation/run.sh
@@ -12,7 +12,7 @@ fi
 
 if ( $# -gt 0 )
 then
-    docker run --name $TASK -it --rm -v $PWD:/$TASK/input $TASK:$VERSION
+    docker.exe run --name $TASK -it --rm -v $PWD:/$TASK/input $TASK:$VERSION
 else
-    docker run --name $TASK -it --rm -v $PWD:/$TASK/input $TASK:$VERSION $@
+    docker.exe run --name $TASK -it --rm -v $PWD:/$TASK/input $TASK:$VERSION $@
 fi
